20fa1e7a528489be3a4efb23f6db6650838487c1
Roshan J <devenforced@gmail.com>
2021-07-13 02:41:01 +0530

    done till ex9 of lesson5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import "./styles.css";
 
 var headingText="Inside Out";
-var col="red"
+// var col="red";
+// var welcomeUser="Teejay"//prompt("Enter username in the prompt command:Teejay");
+// var fC="blue";
+
+
 export default function App() {
+  const [likeCounter,setLikeCounter]=useState(0);
+  function likeClickHandler(){
+    var newlikeCounter=likeCounter+1;
+    setLikeCounter(newlikeCounter);
+  }
+
   return (
     <div className="App">
-      <h1 style={{ backgroundColor:col }}>{headingText}</h1>
+      <h1 /*style={{ backgroundColor:col }}*/>{headingText}</h1>
+      {/* <h2 style={{color:fC}}>{welcomeUser}</h2> */}
+      <button onClick={likeClickHandler}>Like this!</button>{likeCounter}
     </div>
   );
 }
